8070b272069f4d7cf3f1abc2ec6a697d658bb9d1
Felipe Nicoletti Reis Mario <felipenicolettirmario@gmail.com>
2021-02-18 20:00:27 -0300

    Initial Files

---
+++ src/main/java/com/fatec/sp/gov/br/teste/TesteApplication.java
@@ -0,0 +1,13 @@
+package com.fatec.sp.gov.br.teste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fatec/sp/gov/br/teste/TesteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatec.sp.gov.br.teste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
